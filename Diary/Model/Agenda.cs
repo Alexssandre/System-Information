948f1efe297a4c564d9b7151a40f990ca78676ed
Alexssandre <alexssandre.silva@outlook.com>
2022-04-30 21:15:04 -0300

    Primeiro

---
+++ Diary/Model/Agenda.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Diary.Model
+{
+    public class Agenda 
+    {
+        public int Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public int Telefone { get; set; }
+        public int Idade { get; set; } 
+
+        [JsonProperty ("cep")]
+        public int Cep { get; set; }
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+        [JsonProperty("uf")]
+        public string Uf { get; set; }        
+
+
+    }
+}
